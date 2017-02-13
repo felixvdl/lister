8119efda847c8d31f0a1d4b30a97534d3b640b9d
[01;31m[K"name"[m[K: "Felix Vandelanotte <[01;31m[K"email"[m[K: "vandelanottefelix@gmail.com>
2017-02-13 21:06:41 +0100

    finish storing of auth token upon creation and redirecting to next page

--- index.ios.js
+++ index.ios.js
@@ -6,43 +6,53 @@
 // npm --isave json-server
 // json-server db.json
  import React, { Component } from 'react';
- import { AppRegistry, StyleSheet } from 'react-native';
+ import { AppRegistry, StyleSheet, Navigator, Text, View } from 'react-native';
  import { Todo } from './src/app/todo';
  import { Reddit } from './src/app/Reddit'
  import { TodoLists } from './src/app/todoLists'
  import { MainPage } from './src/app/MainPage'
- import { Register }from './src/app/register'
+ import { Register } from './src/app/register'
 
-class TodoApp extends Component {
+
+ class Main extends Component {
+
    renderScene(route, navigator) {
-    console.log(route);
-    // if(route.name == 'root') {
-    //   return <Root navigator={navigator} />
-    // }
-    if(route.name == 'register') {
-      return <Register navigator={navigator} />
-    }
-    // if(route.name == 'login') {
-    //   return <Login navigator={navigator} />
-    // }
-    // if(route.name == 'home') {
-    //   return <Home navigator={navigator} {...route.passProps} />
-    // }
-    // if(route.name == 'update') {
-    //   return <Update navigator={navigator} {...route.passProps} />
-    // }
-  }
+     console.log(route);
+     if(route.name == 'todo') {
+       return <Todo navigator={navigator} />
+     }
+     if(route.name == 'register') {
+       return <Register navigator={navigator} />
+     }
+     if(route.name == 'login') {
+       return <Login navigator={navigator} />
+     }
+     if(route.name == 'home') {
+       return <Home navigator={navigator} {...route.passProps} />
+     }
+     if(route.name == 'update') {
+       return <Update navigator={navigator} {...route.passProps} />
+     }
+   }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Navigator
-          initialRoute={{name: 'register'}}
-          renderScene={this.renderScene.bind(this)}
-        />
-      </View>
-    );
-  }
-}
+   render() {
+     return (
+       <View style={styles.container}>
+         <Navigator
+           initialRoute={{name: 'register'}}
+           renderScene={this.renderScene.bind(this)}
+         />
+       </View>
+     );
+   }
+ }
 
- AppRegistry.registerComponent('Todo', () => TodoApp);
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     backgroundColor: '#F5FCFF',
+   },
+ });
+
+
+ AppRegistry.registerComponent('Todo', () => Main);
